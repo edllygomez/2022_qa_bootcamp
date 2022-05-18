46e3e3eebde94bb13bf36ca9ec5c2ea196d8fed8
jalzatec <jalzate@growthaccelerationpartners.com>
2022-05-18 00:23:48 -0500

    Problem 2

--- juan_javascript_homework.js
+++ juan_javascript_homework.js
@@ -1,5 +1,5 @@
+//Problem 1
 function isMultiple(num){
-    //declaracion de variables
     let sum=0;
     if(num>=0){
         for (let i = num-1; i > 0; i--) {
@@ -15,3 +15,30 @@ function isMultiple(num){
 let num1=0;
 isMultiple (num1);
 
+//Problem 2
+
+function isNarcissistic(num){
+    if(num>0 && !isNaN(num)){
+        let numS = num.toString();
+        let nuevo = Array.from(numS);
+        let sum=0;
+        for(let i = 0; i<nuevo.length; i++){
+            let aux=0;
+            aux = nuevo[i]**nuevo.length;
+            sum+=aux;
+             
+        }
+        if(sum===num){
+            console.log(true);
+        } else console.log(false);
+
+    } else {
+        console.log("Error input invalid, please check");
+    }
+    
+    
+}
+ let num2 = 0;
+ isNarcissistic(num2);
+
+ 
