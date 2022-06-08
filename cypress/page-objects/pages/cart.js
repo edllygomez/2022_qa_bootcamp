6571ac9bc6d8a0d8cf240ddbe1d25fabfe823671
jalzatec <jalzate@growthaccelerationpartners.com>
2022-06-08 00:11:10 -0500

    cypress part 2

--- cypress/page-objects/pages/cart.js
+++ cypress/page-objects/pages/cart.js
@@ -1,10 +1,9 @@
 export class Cart{
+    url = '/cart';
     elements = {
         //Add to cart
         getAddToCartByClassBtn: ()=>cy.get('.added_to_cart'),
         getProductName: ()=>cy.get('td.product-name'),
-        /**url:'/cart',
-        title:'Cart', */
         //Apply coupon
         getInputCouponByIdInp: ()=>cy.get('#coupon_code'),
         getApplyCouponByNameBtn: ()=>cy.get('button[name=apply_coupon]'),
@@ -24,20 +23,8 @@ export class Cart{
         this.elements.getInputCouponByIdInp().type('1461 off');
         this.elements.getApplyCouponByNameBtn().click();
     }
-
-
-
-    //apply coupon
-    /**
-     * cy.get('#coupon_code').type('1461 off');
-     * cy.get('button[name=apply_coupon]').click() 
-     */
 }
 
 export const cartPage = new Cart();
-/**
- * cy.get('.added_to_cart').click();
-    cy.url().should('include', '/cart');
-    cy.get('td.product-name').first().should('includes.text','Belt');
- */
+
 
